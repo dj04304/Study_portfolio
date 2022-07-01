11e5a2d6f248d48f9fc00d0016ddb0c17ead6c1d
dj04304 <dj043045@gmail.com>
2022-07-01 10:05:01 +0900

    포트폴리오 자바스크립트 추가 (사이드 메뉴 이벤트, 이메일)

--- js/index.js
+++ js/index.js
@@ -1,3 +1,41 @@
+const contents = document.querySelectorAll(".content");
+const asideMenu = document.querySelectorAll("aside li")
+const firstTop = contents[0].offsetTop;
+const secondTop = contents[1].offsetTop;
+const thirdTop = contents[2].offsetTop;
+const fourthTop = contents[3].offsetTop;
+
+console.log(contents);
+console.log(asideMenu);
+
+asideMenu[0].onclick = () => {
+    window.scroll({top:firstTop, behavior: 'smooth'})
+}
+
+asideMenu[1].onclick = () => {
+    window.scroll({top:secondTop, behavior: 'smooth'})
+}
+
+asideMenu[2].onclick = () => {
+    window.scroll({top:thirdTop, behavior: 'smooth'})
+}
+
+asideMenu[3].onclick = () => {
+    window.scroll({top:fourthTop, behavior: 'smooth'})
+}
+
+for(let i = 0; i < asideMenu.length; i++){
+    asideMenu[i].onmouseover = () => {
+        asideMenu[i].classList.toggle("aside_menu_link")
+    }
+}
+
+for(let i = 0; i < asideMenu.length; i++){
+    asideMenu[i].onmouseout = () => {
+        asideMenu[i].classList.toggle("aside_menu_link")
+    }
+}
+
 const infoEmail = document.querySelector(".informaition_email");
 const gitAddress = document.querySelector(".git_address");
 const blogAddress = document.querySelector(".blog_address");
@@ -5,6 +43,7 @@ const blogAddress = document.querySelector(".blog_address");
 console.log(infoEmail);
 console.log(gitAddress);
 console.log(blogAddress);
+
 // aboutme email
 infoEmail.onmouseover = () => {
     infoEmail.classList.toggle("informaition_email_link");
@@ -45,4 +84,33 @@ blogAddress.onclick = () => {
     location.href = "https://dochistory.tistory.com/";
 }
 
+//footer sns
+const btmEmail = document.querySelector(".email");
+const btmEmailAddress = document.querySelector(".contact p");
+const faceBook = document.querySelector(".facebook");
+const Instagram = document.querySelector(".insta");
 
+btmEmail.onclick = () => {
+    location.href = "mailto:dj043045@gmail.com";
+}
+
+faceBook.onclick = () => {
+    location.href = "https://ko-kr.facebook.com/";
+}
+
+Instagram.onclick = () => {
+    location.href = "https://www.instagram.com/";
+}
+
+
+btmEmailAddress.onmouseover = () => {
+    btmEmailAddress.classList.toggle("bottom_email_link");
+}
+
+btmEmailAddress.onmouseout = () => {
+    btmEmailAddress.classList.toggle("bottom_email_link");
+}
+
+btmEmailAddress.onclick = () => {
+    location.href = "mailto:dj043045@gmail.com";
+}
